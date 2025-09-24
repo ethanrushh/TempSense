8f3cc4dfbcd522314ba453006a1bb1b9532d0cc4
Ethan Rushbrook <ethanrushbrook@outlook.com>
2025-09-24 19:15:51 +1000

    fix: crash from unable to access detached ancestor

--- EthanRushbrook.TempSense/Components/Widgets/WidgetBase.cs
+++ EthanRushbrook.TempSense/Components/Widgets/WidgetBase.cs
@@ -10,6 +10,7 @@ namespace EthanRushbrook.TempSense.Components.Widgets;
 public abstract class WidgetBase(Guid widgetId) : UserControl
 {
     private EventHandler<TappedEventArgs>? _tapHandler;
+    private WidgetTemplate? _card;
     
     public static readonly StyledProperty<object?> ValueProperty =
         AvaloniaProperty.Register<WidgetBase, object?>(nameof(Value));
@@ -38,25 +39,20 @@ public abstract class WidgetBase(Guid widgetId) : UserControl
                 throw new Exception("Widgets must be inside a PageLayout");
             await page.OnAction(widgetId);
         };
-        
-        // Get the card
-        var card = this.FindAncestorOfType<WidgetTemplate>();
 
-        if (card == null)
-            throw new Exception("Widgets must be inside a WidgetTemplate");
+        _card = this.FindAncestorOfType<WidgetTemplate>()
+                ?? throw new Exception("Widgets must be inside a WidgetTemplate");
 
-        card.Tapped += _tapHandler;
+        _card.Tapped += _tapHandler;
     }
 
     protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
     {
-        // Get the card
-        var card = this.FindAncestorOfType<WidgetTemplate>();
-
-        if (card == null)
-            throw new Exception("Widgets must be inside a WidgetTemplate");
-
-        card.Tapped -= _tapHandler;
+        if (_card != null && _tapHandler != null)
+        {
+            _card.Tapped -= _tapHandler;
+            _card = null; // clear reference to avoid leaks
+        }
         
         base.OnDetachedFromVisualTree(e);
     }
